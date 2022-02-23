2567a2c502b381a5e84e99200698d364a498cab1
woo <choiwu0917@gmail.com>
2022-02-23 21:10:58 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import React,{Suspense, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import loadable from '@loadable/component';
+const SplitMe=loadable(()=>import('./SplitMe'),{fallback:<div>loading...</div>});
 
-function App() {
-  return (
+function App(){
+  const [visible, setvisible]=useState(false);
+  const onClick=()=>{
+    setvisible(true);
+  };
+  const onMouseOver=()=>{
+    SplitMe.preload();
+  } 
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React</p>
+        {visible && <SplitMe/>}
       </header>
     </div>
-  );
+  )
 }
+
 
 export default App;
